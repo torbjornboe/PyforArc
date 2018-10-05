de7f7ca0badfe6f7f183b1270ce5bbb2a5aea6fb
torbjornboe <torbjornable@gmail.com>
2018-10-05 22:17:33 +0200

    last commit before merge with master

--- append.py
+++ append.py
@@ -6,6 +6,9 @@ class BaseValidationError(TypeError):
 class DifferentShapeTypesError(BaseValidationError):
     pass
 
+class FieldmapNotDictError(BaseValidationError):
+    pass
+
 def append(gdb,target,appendfc,fieldmap):
     """
     This tool has no output!
@@ -20,21 +23,12 @@ def append(gdb,target,appendfc,fieldmap):
     if desc_target.shapeType != desc_appendfc.shapeType:
         raise DifferentShapeTypesError
     if type(fieldmap) != dict:
-        print('fieldmap not dict')
-        raise TypeError
+        raise FieldmapNotDictError
     values = list(fieldmap.values())
     keys = list(fieldmap.keys())
     appends = [row for row in arcpy.da.SearchCursor(appendfc,['shape@']+values)]
-    print (appends)
     insertcursor = arcpy.da.InsertCursor(target,['shape@']+keys)
 
     for item in appends:
         insertcursor.insertRow(item)
 
-
-if __name__ == '__main__':
-    gdb = r'C:\Users\torbjorn.boe\Google Drive\Python\PyforArc\tests\testdata.gdb'
-    plants = 'plants_1'
-    trees = 'trees_1'
-    fieldmap = {'tree_diam': 'plant_diam', 'tree_type': 'plant_type'}
-    append(gdb,trees,plants,fieldmap)
