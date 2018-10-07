7ce41761768e5bf683369235506de30ef38ea233
torbjornboe <torbjornable@gmail.com>
2018-10-07 22:35:41 +0200

    finished first version

--- merge.py
+++ merge.py
@@ -1,5 +1,7 @@
 import arcpy
 
+from addmultiplefields import addmultiplefields 
+
 class BaseValidationError(TypeError):
     pass
 
@@ -22,26 +24,40 @@ def merge(gdb,merge1,merge2,fieldmap,outfc,overwrite = False):
     desc_merge2 = arcpy.Describe(merge2)
     if desc_merge1.shapeType != desc_merge2.shapeType:
         raise DifferentShapeTypesError
-    arcpy.CreateFeatureclass_management(gdb,outfc,desc_merge1.shapeType,template=trees, spatial_reference = trees)
+
     if type(fieldmap) != dict:
         print('fieldmap not dict')
         raise TypeError
+    
     values = list(fieldmap.values())
     keys = list(fieldmap.keys())
-    merge1_cursor = arcpy.da.SearchCursor(merge1,['shape@']+keys)
-    merge2_cursor = arcpy.da.SearchCursor(merge2,['shape@']+values)
-    insertcursor = arcpy.da.InsertCursor(outfc,['shape@']+keys)
+    merge1_fields = [(f.name,f.type) for f in arcpy.LitstFields(merge1) if f not in ['OBJECTID','Shape']]
+    merge2_fields = [(f.name,f.type) for f in arcpy.LitstFields(merge2) if f not in ['OBJECTID','Shape']]
+    merge2_out = [f for f in merge2_field if f[0] not in values]#fields from merge2 that is not in fieldmap
+    merge1_restfields = [f for f in merge1_field if f[0] not in keys]
+    keys_type = [f for f in merge1_field if f[0] in keys]
+    values_type = [f for f in merge2_field if f[0] in values]
+    outfields = merge1_restfields + merge2_out + keys_type + values_type
+    arcpy.CreateFeatureclass_management(gdb,outfc, desc_merge1.shapeType, spatial_reference = merge1)
+    addmultiplefields(gdb,outfc,outfields)
+
+    merge1_cursor = arcpy.da.SearchCursor(merge1,['shape@'] + merge1_restfields + keys)
+    merge2_cursor = arcpy.da.SearchCursor(merge2,['shape@'] + merge2_out + vales)
+    
+    insertcursor = arcpy.da.InsertCursor(outfc,['shape@'] + merge1_restfields + keys)
 
     for row in merge1_cursor:
         inserts = [row[0]]
-        for index,fields in enumerate(keys,1):
+        for index,fields in enumerate(merge1_restfields+keys,1):
             inserts.append(row[index])
         insertcursor.insertRow(inserts)
+    del insertcursor
 
+    insertcursor = arcpy.da.InsertCursor(outfc,['shape@'] + merge2_out + values)
 
     for row in merge2_cursor:
         inserts = [row[0]]
-        for index,fields in enumerate(values,1):
+        for index,fields in enumerate(merge2_out + values,1):
             inserts.append(row[index])
         insertcursor.insertRow(inserts)
 
