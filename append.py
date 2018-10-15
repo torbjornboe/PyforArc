f18427dd37ca83f1dfbbfd98403bc548210efdc4
torbjornboe <torbjornable@gmail.com>
2018-10-15 22:06:36 +0200

    transfer all fields not in fieldmap

--- append.py
+++ append.py
@@ -25,9 +25,21 @@ def append(gdb,target,appendfc,fieldmap):
         raise DifferentShapeTypesError
     if type(fieldmap) != dict:
         raise FieldmapNotDictError
+    notwanteds = ['OBJECTID', 'Shape', 'SHAPE']
     values = list(fieldmap.values())
     keys = list(fieldmap.keys())
-    appends = [row for row in arcpy.da.SearchCursor(appendfc,['shape@']+values)]
+    rest_targetfieldsdict = {f.name: f.type for f in arcpy.ListFields(target) if f.name not in notwanteds + keys}
+    rest_appendfieldsdict = {f.name: f.type for f in arcpy.ListFields(target) if f.name not in notwanteds + values}
+    rest_appendfieldslist = []
+    
+    for fname,ftype in rest_appendfieldsdict.items():
+        try:
+            if rest_targetfieldsdict[fname] ==  ftype: #field exist and of same type
+                rest_appendfieldslist.append(fname)
+        except KeyError: # field does not exist
+            pass
+
+    appends = [row for row in arcpy.da.SearchCursor(appendfc,['shape@']+values+rest_appendfieldslist)]
     insertcursor = arcpy.da.InsertCursor(target,['shape@']+keys)
 
     for item in appends:
