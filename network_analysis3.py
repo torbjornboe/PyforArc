1eccb32ace10dc217a00297f4352ba5beb24f767
torboe <torbjornable@gmail.com>
2018-12-27 21:25:00 +0100

    working but not tested args¨

--- network_analysis3.py
+++ network_analysis3.py
@@ -6,11 +6,65 @@ import pathlib
 
 import os
 
-class Od_cost(network, outgdb, origins, destinations, **kwargs):
+class Od_cost:
 
-    def __init__():
+    def __init__(self,network, outgdb, **kwargs):
+
+        def __kvargs_to_full_dict__(kwargs):
+            vals =  {}
+            for i in self.valid_matrixlayer_parameters:
+                try:
+                    vals[i] = kwargs[i]
+                except KeyError:
+                    vals[i] = None
+            return vals
         
-    def get_na_layer_path(descr,alias):
+        self.network = network
+        self.outgdb = outgdb
+        #arcpy.env.overwriteOutput = True
+        arcpy.env.workspace = self.outgdb
+        self.valid_matrixlayer_parameters = ['layer_name', 'travel_mode', 'cutoff', 'number_of_destinations_to_find', 'time_of_day', 'time_zone', 'line_shape', 'accumulate_attributes']
+        if kwargs:
+            self.given_matrixlayer_parameters = __kvargs_to_full_dict__(kwargs)
+            self.layer_name = self.given_matrixlayer_parameters['layer_name']
+            self.travel_mode = self.given_matrixlayer_parameters['travel_mode']
+            self.cutoff = self.given_matrixlayer_parameters['cutoff']
+            self.number_of_destinations_to_find = self.given_matrixlayer_parameters['number_of_destinations_to_find']
+            self.time_of_day = self.given_matrixlayer_parameters['time_of_day']
+            self.time_zone = self.given_matrixlayer_parameters['time_zone']
+            self.line_shape = self.given_matrixlayer_parameters['line_shape']
+            self.accumulate_attributes = self.given_matrixlayer_parameters['accumulate_attributes']
+        self.matrixlayer = arcpy.na.MakeODCostMatrixAnalysisLayer(network, self.layer_name, self.travel_mode, self.cutoff, self.number_of_destinations_to_find, self.time_of_day, self.time_zone, self.line_shape, self.accumulate_attributes)
+        self.odcost_layer = self.matrixlayer.getOutput(0)
+        self.sublayer_names = arcpy.na.GetNAClassNames(self.odcost_layer)
+        self.origins_layer_name = self.sublayer_names["Origins"]
+        self.destinations_layer_name = self.sublayer_names["Destinations"]
+        
+        
+
+
+    def add_origins(self,origins,identifier,search_tolerance):
+        self.origins_identifierfield = identifier
+        self.origins = origins
+        self.origins_search_tolerance = search_tolerance
+        field_mappings = arcpy.na.NAClassFieldMappings(self.odcost_layer, self.origins_layer_name)
+        field_mappings["Name"].mappedFieldName = self.origins_identifierfield
+        arcpy.na.AddLocations(self.odcost_layer, self.origins_layer_name, self.origins, field_mappings,
+                          self.origins_search_tolerance)
+
+    def add_destinations(self,destinations,identifier,searchtolerance):
+        self.destinations = destinations
+        self.destinations_identifierfield = identifier
+        self.destinations_search_tolerance = searchtolerance
+        field_mappings = arcpy.na.NAClassFieldMappings(self.odcost_layer, self.destinations_layer_name)
+        field_mappings["Name"].mappedFieldName = self.destinations_identifierfield
+        arcpy.na.AddLocations(self.odcost_layer, self.destinations_layer_name, self.destinations, field_mappings, self.destinations_search_tolerance)
+
+    def solve(self):
+        arcpy.na.Solve(self.odcost_layer)
+        # except arcgisscripting.ExecuteError ?
+
+    def __get_na_layer_path__(self,descr, alias):
         for item in descr['children']:
             try:
                 if item['aliasName'] == alias:
@@ -18,62 +72,71 @@ class Od_cost(network, outgdb, origins, destinations, **kwargs):
             except KeyError:
                 print('make a base validataion class')
 
-    search_tolerance = 500
-    arcpy.env.overwriteOutput = True
-    arcpy.env.workspace = outgdb
-    #  output_layer_file = os.path.join(output_dir, layer_name + ".lyrx")
-    od_cost = arcpy.na.MakeODCostMatrixAnalysisLayer(network, "testODcostPro")
-    odcost_layer = od_cost.getOutput(0)
+    
 
-    sublayer_names = arcpy.na.GetNAClassNames(odcost_layer)
-    origins_layer_name = sublayer_names["Origins"]
-    destinations_layer_name = sublayer_names["Destinations"]
+    def origins_to_lines(self):
+        descr = arcpy.da.Describe(self.odcost_layer)
+        self.lines_path = self.__get_na_layer_path__(descr,'Lines')
+        
+        self.origings_path = self.__get_na_layer_path__(descr,'Origins')
+        self.origings_path_path = pathlib.Path(self.origings_path)
+        self.origings_path_name = self.origings_path_path.name
+        
+        arcpy.AddXY_management(self.origings_path)
+        #print(origings_path, destinations_path, lines_path)
+        joins.join_1one1(self.origings_path, 'ObjectID', self.outgdb, self.lines_path, 'OriginID', ['Name','POINT_X','POINT_Y'])
+        
+        joins.join_1one1(self.origins,'OBJECTID',self.outgdb,self.lines_path,f'Name_{self.origings_path_name}', from_fields = 'all')
+        
 
-    # arcpy.da.Describe(odcost_layer)
-    #'dataElement': {
-    #		'catalogPath': 'C:\\Users\\torbjorn.boe\\Google Drive\\Python\\PyforArc\\tests\\testdata.gdb\\ODCostMatrix2\\Origins2',
-    #		'FIDSet': None,
-    #		'aliasName': 'Origins',
-    #		'areaFieldName': '',
-    #		'baseName': 'Origins2',
+    def destinations_to_lines(self):
 
-    # test slutt
+        descr = arcpy.da.Describe(self.odcost_layer)
+        self.lines_path = self.__get_na_layer_path__(descr,'Lines')
+        
+        self.destinations_path = self.__get_na_layer_path__(descr,'Destinations')
+        self.destinations_path_path = pathlib.Path(self.destinations_path)
+        self.destinations_path_name = self.destinations_path_path.name
+        
+        arcpy.AddXY_management(self.destinations_path)
+        joins.join_1one1(self.destinations_path, 'ObjectID', self.outgdb, self.lines_path, 'DestinationID', ['Name','POINT_X','POINT_Y'])
+        joins.join_1one1(self.destinations,'OBJECTID',self.outgdb,self.lines_path,f'Name_{self.destinations_path_name}', from_fields = 'all')
+        #arcpy.AddXY_management(self.destinations_path)
 
-    field_mappings = arcpy.na.NAClassFieldMappings(odcost_layer,
-                                                    origins_layer_name)
-    field_mappings["Name"].mappedFieldName = "OBJECTID"
-    arcpy.na.AddLocations(odcost_layer, origins_layer_name, origins, field_mappings,
-                      search_tolerance)
+    def multiple_origins(self, output):
+        #make one point on origin for every line
+        
+        cursor = arcpy.da.InsertCursor(output)
+        pass
 
-    field_mappings = arcpy.na.NAClassFieldMappings(odcost_layer,
-                                                    destinations_layer_name)
-    field_mappings["Name"].mappedFieldName = "OBJECTID"
-    arcpy.na.AddLocations(odcost_layer, destinations_layer_name, destinations,
-                      field_mappings, search_tolerance)
+    def summarize_on_origins(self):
+        #make one point on orgin where line data is summarized
+        pass
 
-    arcpy.na.Solve(odcost_layer)
-
-    descr = arcpy.da.Describe(odcost_layer)
-    origings_path = get_na_layer_path(descr,'Origins')
-    origings_path_path = pathlib.Path(origings_path)
-    origings_path_name = origings_path_path.name
-
-    destinations_path = get_na_layer_path(descr,'Destinations')
-    destinations_path_path = pathlib.Path(destinations_path)
-    destinations_path_name = destinations_path_path.name
-
-    lines_path = get_na_layer_path(descr,'Lines')
-    arcpy.AddXY_management(origings_path)
-    arcpy.AddXY_management(destinations_path)
-
-    print(origings_path, destinations_path, lines_path)
-    joins.join_1one1(origings_path, 'ObjectID', outgdb, lines_path, 'OriginID', ['Name','POINT_X','POINT_X'])
-    joins.join_1one1(destinations_path, 'ObjectID', outgdb, lines_path, 'DestinationID', ['Name','POINT_X','POINT_X'])
-    joins.join_1one1(origins,'OBJECTID',outgdb,lines_path,f'Name_{origings_path_name}', from_fields = 'all')
-    joins.join_1one1(destinations,'OBJECTID',outgdb,lines_path,f'Name_{destinations_path_name}', from_fields = 'all')
-
-
-    # layer_object.saveACopy(output_layer_file)
+        
+##
+##
+##    odcost_layer = od_cost.getOutput(0)
+##
+##    sublayer_names = arcpy.na.GetNAClassNames(odcost_layer)
+##    origins_layer_name = sublayer_names["Origins"]
+##    destinations_layer_name = sublayer_names["Destinations"]
+##
+##    # arcpy.da.Describe(odcost_layer)
+##    #'dataElement': {
+##    #		'catalogPath': 'C:\\Users\\torbjorn.boe\\Google Drive\\Python\\PyforArc\\tests\\testdata.gdb\\ODCostMatrix2\\Origins2',
+##    #		'FIDSet': None,
+##    #		'aliasName': 'Origins',
+##    #		'areaFieldName': '',
+##    #		'baseName': 'Origins2',
+##
+##    # test slutt
+##
+##    
+##
+##    ##
+##
+##    # layer_object.saveACopy(output_layer_file)
 
 
 if __name__ == '__main__':
@@ -81,4 +144,9 @@ if __name__ == '__main__':
     gdb = r'C:\Users\torbjorn.boe\Google Drive\Python\PyforArc\tests\testdata.gdb'
     origins = 'plants'
     destinations = 'trees'
-    od_cost(nd,gdb,origins,destinations,)
+    odcost = Od_cost(nd,gdb,line_shape = 'STRAIGHT_LINES')
+    odcost.add_origins(origins,'OBJECTID',500)
+    odcost.add_destinations(destinations,'OBJECTID',500)
+    odcost.solve()
+    odcost.origins_to_lines()
+    odcost.destinations_to_lines()
