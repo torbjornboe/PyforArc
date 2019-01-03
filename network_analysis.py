84c46d7e5a07ad6ffb7cc4522fe8efc334ef1f94
torboe <torbjornable@gmail.com>
2019-01-03 22:27:11 +0100

    ready for merge. Tests added to  and committed

--- network_analysis.py
+++ network_analysis.py
@@ -128,15 +128,4 @@ class Od_cost:
             insertcursor.insertRow(item)
 
 
-if __name__ == '__main__':
-    nd = r'C:\Users\torbjorn.boe\Google Drive\Python\PyforArc\tests\test_odcost.gdb\test_na\test_na_ND'
-    gdb = r'C:\Users\torbjorn.boe\Google Drive\Python\PyforArc\tests\testdata.gdb'
-    origins = 'plants'
-    destinations = 'trees'
-    odcost = Od_cost(nd,gdb,line_shape = 'STRAIGHT_LINES', travel_mode = 'walktime', cutoff = 100, layer_name = 'testname')
-    odcost.add_origins(origins,'OBJECTID',500)
-    odcost.add_destinations(destinations,'OBJECTID',500)
-    odcost.solve()
-    odcost.origins_to_lines()
-    odcost.destinations_to_lines()
-    odcost.multiple_origins('multipleorigins',gdb)
+
