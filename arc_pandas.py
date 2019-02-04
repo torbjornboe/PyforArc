e4f0da63b0e48e0a5dca742df2ea0dfdd888b705
torboe <torbjornable@gmail.com>
2019-02-04 10:33:45 +0100

    some desperate attempts to get it working

--- arc_pandas.py
+++ arc_pandas.py
@@ -1,5 +1,6 @@
 import arcpy
 import pandas as pd
+import pathlib
 
 import addmultiplefields
 
@@ -20,14 +21,11 @@ def table_to_dataframe(fc, workspace = None, index = None):
     df = pd.DataFrame(data, index = index, columns = fields)
     
     for field in fields:
-        print(field)
-        print(types[field])
-        print(map_tbl_to_df[types[field]])
         df[field].astype(map_tbl_to_df[types[field]])
     return df
     
 
-def dataframe_to_featureclass(dataframe, featureclass, geometry, geometrycolumn = 'SHAPE', workspace = None, spatial_reference = None, overwrite = True):
+def dataframe_to_featureclass(df, featureclass, geometry, geometrycolumn = 'SHAPE', workspace = None, spatial_reference = None, overwrite = True):
     arcpy.env.overwriteOutput = overwrite
     geometry = geometry.lower()
     allowed_geometry = ['polygon','polyline','point']
@@ -42,26 +40,35 @@ def dataframe_to_featureclass(dataframe, featureclass, geometry, geometrycolumn 
             isdir = p.parent.is_dir()
         except TypeError:
             spatial_reference = arcpy.SpatialReference(spatial_reference)
-                    
+
+    
     arcpy.CreateFeatureclass_management(workspace, featureclass, geometry, spatial_reference = spatial_reference)
 
     #dt.kind
     #remember to resolve workspace and name for addmultiplefields
     columns = df.columns
     fields = []
+    #new_columns = []
     reserved = ['OBJECTID', 'SHAPE',]
     for column in columns:
-        if column.upper() not in reserved:
+        if column.upper() in reserved:
+            fields.append((f'{column}_',map_df_to_tbl[df[column].dtype.kind]))
+            #new_columns.append(column)
+        else:
             fields.append((column,map_df_to_tbl[df[column].dtype.kind]))
-	
+            
+            
     addmultiplefields.addmultiplefields(workspace,featureclass,fields)
-    fields = [field.name for field in arcpy.ListFields(featureclass) if field.name not in reserved]
+    fields = [field[0] for field in fields]
+    #print(fields)
     icursor = arcpy.da.InsertCursor(featureclass,fields + ['shape@'])#obs. not working now
     
+    #print(columns)
+    #df = df[new_columns]
     for i,row in df.iterrows():
         if geometry == 'point':
             point = arcpy.Point(row[geometrycolumn][1], row[geometrycolumn][0])
-            print(point)
+            #print(point)
             arc_geometry = arcpy.PointGeometry(point)
         if geometry == 'polygon':
             array = arcpy.Array(row[geometrycolumn])#possibly need to go innto tuple
@@ -69,8 +76,16 @@ def dataframe_to_featureclass(dataframe, featureclass, geometry, geometrycolumn 
         if geometry == 'polyline':
             array = arcpy.Array(row[geometrycolumn])#needs testing.
             arc_geometry = arcpy.Polyline(array)
-        payload = list(row.values) + [arc_geometry]
+        nrow = []
+        for val in row:
+            if type(val) == tuple:
+                val = str(val)
+                print(val)
+            nrow.append(val)
+        #payload = list(row.values) + [arc_geometry]
+        payload = nrow + [arc_geometry]
         print(payload)
+        print(type(payload))
         icursor.insertRow(payload)
             
             
@@ -88,7 +103,7 @@ if __name__ == '__main__':
     gdb = r'C:\Users\torbjorn.boe\Google Drive\Python\PyforArc\tests\testdata.gdb'
     origins = 'plants'
     df = table_to_dataframe(origins,gdb,'OBJECTID')#'OBJECTID'
-    print(df.head())
-    print(df.index)
+##    print(df.head())
+##    print(df.index)
 
-    dataframe_to_featureclass(df, 'df_to_fc', 'POINT', workspace = gdb)
+    dataframe_to_featureclass(df, 'df_to_fc', 'POINT', workspace = gdb, spatial_reference = 25832)
