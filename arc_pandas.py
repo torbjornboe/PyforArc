268a0a64df920b4ba4087152fc22aacd5bce4d87
torboe <torbjornable@gmail.com>
2019-02-21 12:10:40 +0100

    working version 1

--- arc_pandas.py
+++ arc_pandas.py
@@ -1,18 +1,21 @@
+import json
+import pathlib
+
 import arcpy
 import pandas as pd
-import pathlib
 
 import addmultiplefields
 
-map_tbl_to_df = {'String':'U', 'Float': 'f', 'Double': 'f', 'Short': 'i', 'Long':'i', 'Date': 'M', 'OID':'i', 'Geometry':'O'}
+map_tbl_to_df = {'String':'U', 'Float': 'f', 'Double': 'f', 'Short': 'i', 'Long':'i', 'Integer':'i', 'Date': 'M', 'OID':'i', 'Geometry':'O'}
 map_df_to_tbl = {'U': 'String', 'f': 'Float', 'i': 'Long', 'M': 'Date', 'O': 'String'}
-#['OID', 'Geometry', 'String', 'Double']
+
 def table_to_dataframe(fc, workspace = None, index = None):
     if workspace:
         arcpy.env.workspace = workspace
         
     fields = [field.name for field in arcpy.ListFields(fc)]
     types = {field.name: field.type for field in arcpy.ListFields(fc)}
+    #print(types)
     
     if index:
         index = [row[0] for row in arcpy.da.SearchCursor(fc,index)]
@@ -22,11 +25,11 @@ def table_to_dataframe(fc, workspace = None, index = None):
     
     for field in fields:
         df[field].astype(map_tbl_to_df[types[field]])
-    print(df['SHAPE@JSON'])
+    #print(df['SHAPE@JSON'])
     return df
     
 
-def dataframe_to_featureclass(df, featureclass, geometry, geometrycolumn = 'SHAPE', workspace = None, spatial_reference = None, overwrite = True):
+def dataframe_to_featureclass(df, featureclass, geometry, geometrycolumn = 'SHAPE@JSON', workspace = None, overwrite = True):
     geometry = geometry.lower()
     allowed_geometry = ['polygon','polyline','point']
     if geometry not in allowed_geometry:
@@ -35,60 +38,56 @@ def dataframe_to_featureclass(df, featureclass, geometry, geometrycolumn = 'SHAP
     if workspace:
         arcpy.env.workspace = workspace
         arcpy.env.overwriteOutput = overwrite
-    if spatial_reference:
-        try:
-            p = pathlib.Path(spatial_reference)
-            isdir = p.parent.is_dir()
-        except TypeError:
-            spatial_reference = arcpy.SpatialReference(spatial_reference)
 
-    
+    # should implement a copy to avoid potential conflict with existing columns
+    df['__wkid__'] = df['SHAPE@JSON'].apply(lambda x: json.loads(x)['spatialReference']['wkid'])
+    if len(df['__wkid__'].unique()) > 1:
+        print('data containing non-uniqe spatial reference')
+        raise ValueError
+    spatial_reference = arcpy.SpatialReference(df['__wkid__'].unique()[0])
     arcpy.CreateFeatureclass_management(workspace, featureclass, geometry, spatial_reference = spatial_reference)
 
-    #dt.kind
     #remember to resolve workspace and name for addmultiplefields
     columns = df.columns
     fields = []
-    #new_columns = []
     reserved = ['OBJECTID', 'SHAPE',]
     for column in columns:
-        if column.upper() in reserved:
-            fields.append((f'{column}_',map_df_to_tbl[df[column].dtype.kind]))# adding underscore
-            # new_columns.append(column)
-        else:
-            fields.append((column,map_df_to_tbl[df[column].dtype.kind]))
+        if column not in [geometrycolumn]:
+            if column.upper() in reserved:
+                new_col = f'{column}_df'
+                df.rename(columns= {column: new_col}, inplace= True)
+                fields.append((new_col,map_df_to_tbl[df[new_col].dtype.kind]))
+            else:
+                fields.append((column,map_df_to_tbl[df[column].dtype.kind]))
             
             
     addmultiplefields.addmultiplefields(workspace,featureclass,fields)
     fields = [field[0] for field in fields]
-    #print(fields)
-    icursor = arcpy.da.InsertCursor(featureclass,fields + ['shape@'])#obs. not working now
-    
-    #print(columns)
-    #df = df[new_columns]
-    for i,row in df.iterrows():
+    icursor = arcpy.da.InsertCursor(featureclass,fields + ['shape@'])
+
+    for i,row in df[fields].iterrows():
         if geometry == 'point':
-            point = arcpy.Point(row[geometrycolumn][0], row[geometrycolumn][1])
-            #print(point)
-            arc_geometry = arcpy.PointGeometry(point)
+            geometrydata = json.loads(df.loc[i,geometrycolumn])
+            point = arcpy.Point(geometrydata['x'], geometrydata['y'])
+            arc_geometry = [arcpy.PointGeometry(point)]
         if geometry == 'polygon':
-            print(row[geometrycolumn])
-            array = arcpy.Array(row[geometrycolumn])#possibly need to go innto tuple
-            # arc_geometry = arcpy.Polygon(array)
-            arc_geometry = row['shape@']
+            geometrydata = json.loads(df.loc[i,geometrycolumn])
+            arc_geometry = []
+            for feature in geometrydata['rings']:
+                arc_geometry.append(arcpy.Polygon(arcpy.Array([arcpy.Point(*coords) for coords in feature])))
         if geometry == 'polyline':
-            array = arcpy.Array(row[geometrycolumn])#needs testing.
-            arc_geometry = arcpy.Polyline(array)
+            geometrydata = json.loads(df.loc[i,geometrycolumn])
+            arc_geometry = []
+            for feature in geometrydata['paths']:
+                arc_geometry.append(arcpy.Polyline(arcpy.Array([arcpy.Point(*coords) for coords in feature])))
         nrow = []
         for val in row:
             if type(val) == tuple:
                 val = str(val)
-                print(val)
             nrow.append(val)
-        #payload = list(row.values) + [arc_geometry]
-        payload = nrow + [arc_geometry]
-        print(payload)
-        print(type(payload))
+        payload = nrow + arc_geometry
+        #print(payload)
+        #print(type(payload))
         icursor.insertRow(payload)
             
             
@@ -102,12 +101,17 @@ def dataframe_to_featureclass(df, featureclass, geometry, geometrycolumn = 'SHAP
 
     
     
-if __name__ == '__main__':
-    gdb = r'C:\Users\torbjorn.boe\Google Drive\Python\PyforArc\tests\testdata.gdb'
-    origins = 'plants_poly'
-    df = table_to_dataframe(origins,gdb,'OBJECTID')#'OBJECTID'
-##    print(df.head())
+#if __name__ == '__main__':
+    #gdb = r'C:\Users\torbjorn.boe\Google Drive\Python\PyforArc\tests\testdata.gdb'
+ #   gdb = r'C:\temp_data\Odc_framtidig.gdb'
+    #origins = 'plants_line'
+    #df = table_to_dataframe(origins,gdb,'OBJECTID')#'OBJECTID'
+  #  fc = 'F_Gange_30_min_Hestevanen'
+    #df = table_to_dataframe(origins, gdb, 'OBJECTID')  # 'OBJECTID'
+   # df = table_to_dataframe(fc, gdb, 'OBJECTID')  # 'OBJECTID'
+
+    #print(df.head())
 ##    print(df.index)
-    df['superdiam'] = df['plant_diam'] * 5
-    df.to_csv(r'C:\temp_data\df_to_poly2.csv')
-    #dataframe_to_featureclass(df, 'df_to_fc_poly2', 'POLYGON', workspace = gdb, spatial_reference = 25832)
+    # df['superdiam'] = df['plant_diam'] * 5
+    # df.to_csv(r'C:\temp_data\df_to_poly2.csv')
+    # dataframe_to_featureclass(df, 'df_to_fc_LINE', 'POLYLINE', workspace = gdb)
